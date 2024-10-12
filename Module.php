a30dc1e446ea856da074498d6b173e0510513063
DarkPh0eNixKrg <fatrus.s0dtl@gmail.com>
2024-10-12 10:31:46 +0500

    Fixed bug in Monitoring -> Maps
    Fixed bug in Monitoring -> Maps when refreshed data about hosts

--- Module.php
+++ Module.php
@@ -12,7 +12,11 @@ class Module extends CModule {
         $router = clone APP::Component()->get('router');
         $layout = $router->getLayout();
         if ($action_page) {
-            if ($action_page != 'jsrpc.php' && $layout != 'layout.widget' && $layout != 'layout.json') {
+            if ($action_page != 'jsrpc.php'
+                && $layout != 'layout.widget'
+                && $layout != 'layout.json'
+                && $action_page != 'map.php'  
+            ) {
                 echo '<script type="text/javascript">';
                 echo file_get_contents(__DIR__.'/js/custom_menupopup.js');
                 echo '</script>';
